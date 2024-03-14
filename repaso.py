1bdd7668fc6378933d99193f546d8e4d1b95a5be
noeliagonzalez29 <ngonzalezg31@educarex.es>
2024-03-14 12:47:56 +0100

    cambios en rama

--- repaso.py
+++ repaso.py
@@ -1,7 +1,6 @@
 print('Hola')
 num=0
 input('introduce un numero')
-if num%2==0:
-    print('el numero es par')
-else:
-    print ('el numero es impar')
+if num%2==1:
+    print('el numero es impar')
+
